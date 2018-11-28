b92d81a0d7a69b3d23880fe17351454f03a71064
initdb <benedict.schwind@live.de>
2018-11-28 15:15:55 +0100

    impl. reading and creation of new element.

--- coldfire/Project_Headers/linkedList.h
+++ coldfire/Project_Headers/linkedList.h
@@ -8,6 +8,15 @@
 #ifndef LINKEDLIST_H_
 #define LINKEDLIST_H_
 
+#include "UART0.h"
+#include "support_common.h"  // include peripheral declarations and more; 
+#include "uart_support.h"    // universal asynchronous receiver transmitter,
+                             // (d.h. die serielle Schnittstelle)
+#include "terminal_wrapper.h"
+//#include "malloc_wrapper.h"
+
+#include <stdlib.h>
+
 void linked_list();
 
 #endif /* LINKEDLIST_H_ */
--- coldfire/Sources/linkedList.c
+++ coldfire/Sources/linkedList.c
@@ -19,49 +19,65 @@ void linked_list()
 	asm
 	{
 		schleife:  
-
-	    // Zahl einlesen (Ende vereinfachend mit Wert 0)
-	    //----------------------------------------------
-	 
-
-	    // Einzuhngendes Element aufbauen
-	    // -------------------------------
-	    // Mit malloc; malloc erwartet den Parameter im Register D0,
-		// weil dafr "register_abi" gilt (nicht compact_abi)
-
-
-		//                  ___________ 
-		//          a0 -->  |zahl|  0 | 
-		//              
-	             
-
-		// Organisation der Listeniteration
-		//
-		// a2 zeigt auf das aktuelle Element (oder 0  Listenende)
-		// a3 zeigt   a u f   d e n   Z e i g e r   im vorhergehenden Element
-		//
-		//                              a3         a2 
-		//                                \         \
-		//  anker:                         \         \
-		//  ______     ___________     _____V_____    V___________     ___________
-		//  |    | --> |zahl|next| --> |zahl|next| --> |zahl|next| --> |zahl|  0 |
-		//  
-		//
-		//    Funtioniert 
-		//       + auch am Listenende (a3 zeigt auf 0, a2 ist 0)
-		//       + auch bei Einfgen vor dem ersten Element 
-		//                           (a3 zeigt auf Anker, a2 auf das erste Element
-		//       + auch bei leerer Liste (a3 zeigt auf Anker, Anker und a2 sind 0)
-
-		// suchen der Einfgestelle
-		//-------------------------
-
-		// Iterator-Prchen initialisieren
-		naechstes:      
-
-		// Einfgestelle gefunden: Objekt einhngen
-		//-----------------------------------------                
-		gefunden:
+			// clear D0 reg of artefacts
+			CLR.L D0
+			
+			// print out msginput str
+			PEA msginput
+			JSR TERM_WriteString
+			ADDA.L #4,SP
+			
+			// Zahl einlesen (Ende vereinfachend mit Wert 0)
+			//----------------------------------------------
+			// Read integer from Terminal
+			JSR INOUT_ReadInt
+			
+			TST.L D0 //test longword D0 is Null
+			BEQ ausgabe //if prior tst cmd 0 -> jump to ausgabe 
+			
+			MOVE.L D0,D7			
+			// Einzufuegendes Element aufbauen
+			// -------------------------------
+			// Mit malloc; malloc erwartet den Parameter im Register D0,
+			// weil dafuer "register_abi" gilt (nicht compact_abi)
+			MOVE.L #6,D0
+			JSR malloc
+			MOVE.W D7,(A0)
+	
+			//         +----+----+ 
+			// a0 -->  |zahl|  0 | 
+			//         +----+----+
+					 
+	
+			// Organisation der Listeniteration
+			//
+			// a2 zeigt auf das aktuelle Element (oder 0  Listenende)
+			// a3 zeigt   a u f   d e n   Z e i g e r   im vorhergehenden Element
+			//
+			//                              a3         a2 
+			//                                \         \
+			//  anker:                         \         \
+			//  ______     ___________     _____V_____    V___________     ___________
+			//  |    | --> |zahl|next| --> |zahl|next| --> |zahl|next| --> |zahl|  0 |
+			//  
+			//
+			//    Funtioniert 
+			//       + auch am Listenende (a3 zeigt auf 0, a2 ist 0)
+			//       + auch bei Einfgen vor dem ersten Element 
+			//                           (a3 zeigt auf Anker, a2 auf das erste Element
+			//       + auch bei leerer Liste (a3 zeigt auf Anker, Anker und a2 sind 0)
+	
+			// suchen der Einfgestelle
+			//-------------------------
+	
+			// Iterator-Praechen initialisieren
+				LEA anker,A3
+				MOVE.L (A3),A2
+			naechstes:      
+	
+			// Einfgestelle gefunden: Objekt einhngen
+			//-----------------------------------------                
+			gefunden:
 
 				
 		bra schleife         // naechste Zahl
