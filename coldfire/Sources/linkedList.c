1422552bd8640c016eded2e4932de2a7921c5aee
initdb <benedict.schwind@live.de>
2018-12-05 15:06:12 +0100

    continue impl. broken atm

--- coldfire/Sources/linkedList.c
+++ coldfire/Sources/linkedList.c
@@ -8,7 +8,10 @@
 #include "linkedList.h"
 
 char msginput[]  = "\r\nZahl eingeben (0 fuer Ende): "; 
-char msgfehler[] = "\nFehler bei malloc() !\n";
+char msgerror[] = "\nFehler bei malloc() !\n";
+char msgout[] = "\r\nSortierte Liste:\r\n";
+char msgdelimiter[] = "->";
+char msgend[] = "ende.\r\n";
 
 void *anker = NULL;
 
@@ -32,7 +35,7 @@ void linked_list()
 			// Read integer from Terminal
 			JSR INOUT_ReadInt
 			
-			TST.L D0 //test longword D0 is Null
+			TST.W D0 //test longword D0 is Null
 			BEQ ausgabe //if prior tst cmd 0 -> jump to ausgabe 
 			
 			MOVE.L D0,D7			
@@ -44,6 +47,10 @@ void linked_list()
 			JSR malloc
 			MOVE.W D7,(A0)
 	
+			TST.L A0        // a0 enthaelt Zeiger auf neues Objekt
+			BEQ	fehler
+			
+			CLR.L 2(A0)		// Bereinigen der letzten 4Byte des neuen Elements
 			//         +----+----+ 
 			// a0 -->  |zahl|  0 | 
 			//         +----+----+
@@ -70,22 +77,69 @@ void linked_list()
 			// suchen der Einfgestelle
 			//-------------------------
 	
-			// Iterator-Praechen initialisieren
-				LEA anker,A3
-				MOVE.L (A3),A2
+			// Iterator-Paerchen initialisieren
+			LEA anker,A3
+			MOVE.L (A3),A2
+			//LEA anker,A3
 			naechstes:      
-	
-			// Einfgestelle gefunden: Objekt einhngen
+				// finden der einfuege Stelle
+				// Fall 1: Element nicht in Liste
+				TST.L A2		// test ob A2 = NULL
+				BEQ gefunden	// falls ja Ende der Listes
+				
+				// Fall 2: Element vergleichen
+				MOVE.W (A2),D0
+				EXT.L D0		// !! EXT: sign extend
+				EXT.L D7		// !!
+				CMP.L D0,D7		// compare nur mit .L (32 bit) words
+				BLE gefunden
+				
+				// weiterschalten des Iterator-Paerchen
+				LEA 2(A2),A3
+				MOVE.L (A3),A2
+			BRA naechstes
+			// Einfuegestelle gefunden: Objekt einhaengen
 			//-----------------------------------------                
 			gefunden:
+				MOVE.L A0,(A3)
+				MOVE.L A2,2(A0)
 
-				
 		bra schleife         // naechste Zahl
       
 		// gesamte Liste ausgeben
 		//-----------------------
-		ausgabe: 
-
+		ausgabe:
+			// print out msgout str
+			PEA msgout
+			JSR TERM_WriteString
+			ADDA.L #4,SP
+			
+			// printout each element of the list
+			LEA anker,A2
+			outputloop:
+				TST.L A2		// test ob A2 = NULL
+				BEQ ende		// falls ja Ende der Listes
+				// printout
+				MOVE.W (A2),-(SP)
+				JSR INOUT_WriteInt
+				ADDA.L #2,SP
+				
+				PEA msgdelimiter
+				JSR TERM_WriteString
+				ADDA.L #4,SP
+				// set iterator to next one
+				MOVE.L 2(A2),A2
+			BRA outputloop
+		
+		fehler:	//Fehler behandlung eigentlich nur Ausgabe
+			PEA	msgerror 
+			jsr	TERM_WriteString	
+			adda #4, SP
+		bra ende
+			
 		ende:
+			PEA	msgend
+			jsr	TERM_WriteString	
+			adda #4, SP
 	}
 }
