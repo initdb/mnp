f6546672da870df70e744d6b6c782d07ef593695
initdb <benedict.schwind@live.de>
2018-12-12 20:02:52 +0100

    fix setting of leds

--- coldfire/Sources/ledbtn.c
+++ coldfire/Sources/ledbtn.c
@@ -18,16 +18,16 @@
  ********************************************/
 #define MEMMAP_OFF	0x40000000
 // port pin assignment registers
-#define PNQPAR		MEMMAP_OFF + 0x100068
-#define PTCPAR		MEMMAP_OFF + 0x10006f
+#define PNQPAR		(MEMMAP_OFF + 0x100068)
+#define PTCPAR		(MEMMAP_OFF + 0x10006f)
 // port data direction registers
-#define DDRNQ		MEMMAP_OFF + 0x100020
-#define DDRTC		MEMMAP_OFF + 0x100027
+#define DDRNQ		(MEMMAP_OFF + 0x100020)
+#define DDRTC		(MEMMAP_OFF + 0x100027)
 // port pin data/ set data registers
-#define PORTNQ		MEMMAP_OFF + 0x100038
-#define PORTTC		MEMMAP_OFF + 0x10003c
+#define PORTNQ		(MEMMAP_OFF + 0x100038)
+#define PORTTC		(MEMMAP_OFF + 0x10003f)
 // port clear output data
-#define CLRTC		MEMMAP_OFF + 0x100057
+#define CLRTC		(MEMMAP_OFF + 0x100057)
 
 void btn_set_on_press()
 {
@@ -71,30 +71,29 @@ void btn_set_on_press()
 	  	
 		CLR.L D0
 		MOVE.B DDRTC,D0
-		ANDI.L #0xf,D0
+		ORI.L #0xf,D0
 		MOVE.B D0,DDRTC
 
 
 	    loop:
 			// read current btn value
-			/*MOVE PORTNQ,D0
+			CLR.L D0
+	    	MOVE.B PORTNQ,D0
 			ANDI.L #0x20,D0
-			LSR.L #5,D0
+			//LSR.L #5,D0
 			
-			MOVE.L #1,D1
+			MOVE.L #0x20,D1
 			CMP.L D0,D1
-			BEQ setled*/
-			// unset LEDs 
-			//MOVE.L #0x00,D2
-	    	//CLR.L D2
-			MOVE.B D2,CLRTC
-
+			BEQ setled
+			// set LEDs to high
+			MOVE.B #0x0f,D2
+			MOVE.B D2,PORTTC
 	    bra loop
 	    
 	    setled:
-	    	// set LEDs
-	    	//MOVE.L #0x000f,D2
-	    	//MOVE.L D2,PORTTC
+	    	// set LEDs to low
+	    	MOVE.L #0x00,D2
+	    	MOVE.B D2,CLRTC
 	    bra loop
 	}
 }
